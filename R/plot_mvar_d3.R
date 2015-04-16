62ee1a6039a66f4660824b0fedce792d309bca6f
Kris Sankaran <krissankaran@DN0a22c1f8.SUNet>
2015-04-15 17:51:08 -0700

    R interface for htmlwidgets.

---
+++ R/plot_mvar_d3.R
@@ -0,0 +1,33 @@
+#' Interactive multivariate analysis plots
+#'
+#' @import htmlwidgets
+#'
+#' @export
+plot_mvar_d3 <- function(mvar_object, rescaling_ix = 2, rescaling_ref = 1,
+                         width = 800, height = 800) {
+
+  x <- list()
+  for(table_ix in length(mvar_object@table):1) {
+    cur_coord <- mvar_object@table[[table_ix]]@coord
+    colnames(cur_coord) <- paste0("axis", 1:ncol(cur_coord))
+    cur_ann <- mvar_object@table[[table_ix]]@annotation
+
+    # Rescale coordinates to a reference, if desired
+    if(table_ix %in% rescaling_ix) {
+      ref_coord <- mvar_object@table[[rescaling_ref]]@coord
+      cur_coord <- scale_to_max_radius(ref_coord, cur_coord)
+    }
+
+    # Combined df of both coordinates and annoation
+    x[[table_ix]] <- cbind(cur_coord, cur_ann)
+
+  }
+
+  htmlwidgets::createWidget(
+    name = "plot_mvar_d3",
+    x,
+    width = width,
+    height = height,
+    package = "mvarVis"
+  )
+}
