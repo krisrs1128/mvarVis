ca18c55a6f95378818fcae889a4f46d33f7e8070
Kris Sankaran <krissankaran@DN0a22c23f.SUNet>
2015-05-29 13:07:13 -0700

    Simplify plot_mvar code.

--- R/plot_mvar.R
+++ R/plot_mvar.R
@@ -1,152 +1,93 @@
+
+#' @title Supply Default Table Plotting Options
+#'
+#' @param opts A list of plotting options, for which we need to merge
+#'    unspecified elements.
+#' @return opts An updated list of plotting options.
+#'
+#' @export
+merge_table_plot_opts <- function(opts = list()) {
+  if(is.null(opts$aes_list)) opts$aes_list <- list()
+  if(is.null(opts$layers_list)) opts$layers_list <- list()
+
+  default_aes_list <- list(x = "axis_1", y = "axis_2", col = NULL,
+                           shape = NULL, size = NULL, label = NULL)
+  opts$aes_list <- modifyList(default_aes_list, opts$aes_list)
+  default_layers_list <- list(point = TRUE, text = FALSE, arrow = FALSE)
+  opts$layers_list <- modifyList(default_layers_list, opts$layers_list)
+  return (opts)
+}
+
+#' @title Plot an element of class mvarAxis
+#'
+#' @param table_slot An object of class mvarAxis
+#' @param opts A list of lists, giving directions for the aesthetic mapping.
+#'    We expect three component lists, though they will be filled in with
+#'    default values, so they only need to be partially specified.
+#'      $layers_list: A list whose names are any of "point", "text", and "arrow",
+#'        and whose corresponding values are TRUE if that layer is to be added
+#'        to the plot and FALSE otherwise.
+#'      $aes_list: A list whose names are any of "col", "shape", "size", or
+#'        "text", and whose values are the names of the coordinates or
+#'        annotation columns to be used in defining aesthetics.
+#'      $facet_vector: A vector of annotation column names to be used in
+#'        in faceting.
+#'
+#' @return p A ggplot object mapping the layers specified in the arguments.
+#'
+#' @importFrom grid arrow unit
+#'
+#' @export
+plot_table <- function(table_slot, opts = list(), p = ggplot()) {
+  opts <- merge_table_plot_opts(opts)
+  data <- cbind(table_slot@annotation, table_slot@coord)
+
+  opts$aes_list <- opts$aes_list[!sapply(opts$aes_list, is.null)]
+  table_aes <- do.call(aes_string, opts$aes_list)
+  if(opts$layers_list$point) {
+    p  <- p + geom_point(data = data, table_aes)
+  }
+  if(opts$layers_list$text) {
+    p <- p + geom_text(data = data, table_aes)
+  }
+  if(opts$layers_list$arrow) {
+    table_aes$xend  <- table_aes$x
+    table_aes$yend <- table_aes$y
+    table_aes$x  <- 0
+    table_aes$y  <- 0
+    p <- p + geom_segment(data = data, table_aes, arrow = arrow(length = unit(0.5, "cm")))
+  }
+  if(!is.null(opts$facet_vector)) {
+    facet_fmla <- formula(paste0(opts$facet_vector, collapse = "~"))
+    p <- p + facet_grid(facet_fmla)
+  }
+  return (p)
+}
+
 #' @title Plot an mvar object
 #'
 #' @description \code{plot_mvar} helps plot multiple projection layers
 #' associated with an mvar object, using \code{ggplot2} with various options to
 #' customize the plot's appearance.
 #'
-#' @param mvar_object An object of class \code{mvarTable} that we want to
-#'    visualize. Each \code{mvarAxis} component can be visualized as a
-#'    different \code{ggplot2} layer; the \code{coord} slot is needed to provide
-#'    the point positions while the \code{annotation} slot is used to map
-#'    aesthetics to these points.
-#' @param point_ix A vector giving the indices of the axes in the
-#'    \code{mvar_object}'s \code{tables} component that we want to plot as
-#'    points. Specifically, for each element $i$ of \code{point_ix}, we look up
-#'    the i^th element in the \code{mvar_object} \code{table} slot, extract the
-#'     \code{coord} slot, and plot each row as a separate point
-#' @param text_ix A vector analogous to \code{point_ix}, execpt that it gives
-#'    the elements that we plot as text labels. The actual text that appears
-#'    will default to the \code{$label} element in the \code{annotation} slot,
-#'    but this can be customized using the \code{text_vars} component.
-#' @param arrow_ix A vector analogous to \code{point_ix} and \code{text_ix},
-#'    giving the elements for which we plot arrows connecting the origin to
-#'    the \code{coord} position.
-#' @param rescaling_ix When making a biplot, the scales of the two sets of
-#'    points aren't always comparable -- only the *directions* of the
-#'    projections of feature variables are meaningful. This vector indexes the
-#'    tables whose coordinate values we will rescale to match those of a
-#'    reference projection's values, see \code{rescaling_ref}.
-#' @param rescaling_ref The index within the \code{mvar_object} \code{table}
-#'    list of the projection to use as a reference when rescaling other
-#'    projection values. For example, in a biplot including samples and
-#'    features, we could plot the actual values for the samples, and scale the
-#'    feature coordinates to be about the same magnitude as the samples.
-#' @param text_vars A list the same length as the \code{mvar_object}
-#'    \code{table} list whose i^th element either (1) gives the column name in
-#'    the associated annotation table to which map to text, or (2) is NULL,
-#'    meaning that table does not have a text label.
-#' @param proj_axes Which columns in each \code{coord} object should we use to
-#'    do the projections? Defaults to 1-2, the two axes explaining the most
-#'    variability.
-#' @param color_vars A list with the same format as \code{text_vars}, but for
-#'    mapping color aesthetics of each layer.
-#' @param point_size_vars A list with the same format as \code{text_vars}, but
-#'    for mapping the point size aesthetics of each layer.
-#' @param text_size_vars A list with the same format as \code{text_vars}, but
-#'    for mapping the text size aesthetics of each layer.
-#' @param shape_vars A list with the same format as \code{text_vars}, but for
-#'    mapping the shape aesthetic of each layer.
+#' @param mvar_object An object of class \code{mvarAxis} that we want to
+#'    visualize.
+#' @param opts A list whose i^th component contains the opts argument for the
+#'    i^th table in the \code{mvar_object}. See \code{plot_table()} for a
+#'    description of what these need to contain.
 #'
 #' @return p A ggplot object mapping the layers specified in the arguments.
 #'
 #' @importFrom ggplot2 geom_point aes_string geom_segment geom_text ggtitle ggplot
-#' @importFrom grid arrow unit
 #'
 #' @export
-plot_mvar <- function(mvar_object,
-                      point_ix = 1:length(mvar_object@table),
-                      text_ix = numeric(0),
-                      arrow_ix = 2,
-                      rescaling_ix = 2,
-                      rescaling_ref = 1,
-                      text_vars = rep(list(NULL), length(mvar_object@table)),
-                      proj_axes = 1:2,
-                      color_vars = rep(list(NULL), length(mvar_object@table)),
-                      point_size_vars = rep(2, length(mvar_object@table)),
-                      text_size_vars = rep(list(NULL), length(mvar_object@table)),
-                      shape_vars = rep(list(NULL), length(mvar_object@table))) {
-  p <- ggplot()
-  proj_axes <- paste0("axis", proj_axes)
-
-  # Loop over every projection in mvar_object@table, and add the relevant
-  # aesthetic layers
-  for(table_ix in length(mvar_object@table):1) {
-    cur_coord <- mvar_object@table[[table_ix]]@coord
-    colnames(cur_coord) <- paste0("axis", 1:ncol(cur_coord))
-    cur_ann <- mvar_object@table[[table_ix]]@annotation
-
-    # Rescale coordinates to a reference, if desired
-    if(table_ix %in% rescaling_ix) {
-      ref_coord <- mvar_object@table[[rescaling_ref]]@coord
-      cur_coord <- scale_to_max_radius(ref_coord, cur_coord)
-    }
-
-    # Combined df of both coordinates and annoation
-    cur_df <- cbind(cur_coord, cur_ann)
-
-    # Add a point layer for current data, if desired
-    if(table_ix %in% point_ix) {
-
-      # Point size is either a number meaning the literal point size, or a
-      # string specifying the column in the current annotation table to which
-      # we map point sizes
-      if(!is.numeric(point_size_vars[[table_ix]])) {
-        point_map <- geom_point(data = cur_df,
-                                aes_string(x = proj_axes[1],
-                                           y = proj_axes[2],
-                                           col = color_vars[[table_ix]],
-                                           shape = shape_vars[[table_ix]],
-                                           size = point_size_vars[[table_ix]]))
-      } else {
-        point_map <- geom_point(data = cur_df,
-                                aes_string(x = proj_axes[1],
-                                           y = proj_axes[2],
-                                           col = color_vars[[table_ix]],
-                                           shape = shape_vars[[table_ix]]),
-                                size = point_size_vars[[table_ix]])
-      }
+plot_mvar <- function(mvar_object, opts) {
+  for(cur_table in 1:length(mvar_object@table)) {
+    if(cur_table == 1) {
+      p <- plot_table(mvar_object@table[[cur_table]], opts[[cur_table]])
     } else {
-        point_map <- NULL
+      p <- plot_table(mvar_object@table[[cur_table]], opts[[cur_table]], p)
     }
-
-    # Add a text layer for current data, if desired
-    if(table_ix %in% text_ix) {
-      if(!is.numeric(text_size_vars[[table_ix]])) {
-        text_map <- geom_text(data = cur_df,
-                              aes_string(x = proj_axes[1],
-                                         y = proj_axes[2],
-                                         label = text_vars[[table_ix]],
-                                         col = color_vars[[table_ix]],
-                                         shape = shape_vars[[table_ix]],
-                                         size = text_size_vars[[table_ix]]))
-      } else {
-        text_map <- geom_text(data = cur_df,
-                              aes_string(x = proj_axes[1],
-                                         y = proj_axes[2],
-                                         label = text_vars[[table_ix]],
-                                         col = color_vars[[table_ix]],
-                                         shape = shape_vars[[table_ix]]),
-                                size = text_size_vars[[table_ix]])
-      }
-    } else {
-      text_map <- NULL
-    }
-
-    # Add an arrow layer for the current data, if desired
-    if(table_ix %in% arrow_ix) {
-      arrow_map <- geom_segment(data = cur_df,
-                                aes_string(x = 0, y = 0,
-                                           xend = paste0(".9 * ", proj_axes[1]),
-                                           yend = paste0(".9 * ", proj_axes[2]),
-                                           col = color_vars[[table_ix]],
-                                           shape = shape_vars[[table_ix]]),
-                                arrow = arrow(length = unit(0.5, "cm")))
-    } else {
-      arrow_map <- NULL
-    }
-
-    # Append the layers for the current projection
-    p <- p + text_map + arrow_map + point_map
   }
   return (p)
 }
