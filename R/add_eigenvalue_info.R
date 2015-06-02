edf34f440e4893ce177142280758b4b8b9c11cd9
Kris Sankaran <krissankaran@DN0a22c85d.SUNet>
2015-06-01 19:00:14 -0700

    Move tab for eigenvalue check.

--- R/add_eigenvalue_info.R
+++ R/add_eigenvalue_info.R
@@ -10,15 +10,14 @@
 #'  ratio fixed by the ratio of eigenvalues.
 add_eigenvalue_info <- function(p, opts = list()) {
   # add eigenvalue labels for the axis specified by the first element in the aes list
-    merged_aes <- merge_table_plot_opts(opts)$aes_list
-    x_axis  <- as.numeric(gsub("axis_", "", merged_aes$x))
-    y_axis <- as.numeric(gsub("axis_", "", merged_aes$y))
-    eigs_prop <- mvar_object@eig[c(x_axis, y_axis)] / sum(mvar_object@eig)
-    x_label <- sprintf("%s [%g%%]", merged_aes$x, 10 * round(eigs_prop[1], 3))
-    y_label <- sprintf("%s [%g%%]", merged_aes$y, 10 * round(eigs_prop[2], 3))
-    p <- p + scale_x_continuous(x_label) +
-      scale_y_continuous(y_label) +
-      coord_fixed(ratio = eigs_prop[2] / eigs_prop[1])
-  }
+  merged_aes <- merge_table_plot_opts(opts)$aes_list
+  x_axis  <- as.numeric(gsub("axis_", "", merged_aes$x))
+  y_axis <- as.numeric(gsub("axis_", "", merged_aes$y))
+  eigs_prop <- mvar_object@eig[c(x_axis, y_axis)] / sum(mvar_object@eig)
+  x_label <- sprintf("%s [%g%%]", merged_aes$x, 10 * round(eigs_prop[1], 3))
+  y_label <- sprintf("%s [%g%%]", merged_aes$y, 10 * round(eigs_prop[2], 3))
+  p <- p + scale_x_continuous(x_label) +
+    scale_y_continuous(y_label) +
+    coord_fixed(ratio = eigs_prop[2] / eigs_prop[1])
   return (p)
 }
