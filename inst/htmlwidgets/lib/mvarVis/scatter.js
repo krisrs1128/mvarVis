6b05de17f973f29224f542d4b78e8dbbd43378e9
Kris Sankaran <sankaran.kris@gmail.com>
2015-12-14 20:15:33 -0800

    add hover effect for text

--- inst/htmlwidgets/lib/mvarVis/scatter.js
+++ inst/htmlwidgets/lib/mvarVis/scatter.js
@@ -67,7 +67,30 @@ drawText = function(x, index, colInfo) {
 	.attr({
 	    x: function(d) { return setup.scales.xScale(d.axis1); },
 	    y: function(d) { return setup.scales.yScale(d.axis2); },
-	    fill: function(d) { return colInfo.colorScale(d[colInfo.curCol]); }
+	    fill: function(d) { return colInfo.colorScale(d[colInfo.curCol]); },
+	    opacity: .7,
+	    "font-size": 13,
+	    index: index
+	});
+
+    // define interactivity for the circles
+    d3.selectAll("text")
+	.on("mouseover", function(d) {
+	    d3.select(this)
+		.transition()
+		.duration(75)
+		.attr({"font-size": 16,
+		       opacity: 1});
+	    hoverTable(d, d3.select(this).attr("index"));
+	});
+    d3.selectAll("text")
+	.on("mouseout", function(d) {
+	    d3.select(this)
+		.transition()
+		.duration(75)
+		.attr({opacity: .7,
+		       "font-size": 13,
+		      })
 	});
 }
 
