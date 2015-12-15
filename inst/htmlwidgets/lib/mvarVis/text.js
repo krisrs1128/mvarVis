006c81dfdb75687373a61e3ab08d63e0c0514708
Kris Sankaran <sankaran.kris@gmail.com>
2015-12-14 22:36:44 -0800

    select individual text elements (not whole group)

--- inst/htmlwidgets/lib/mvarVis/text.js
+++ inst/htmlwidgets/lib/mvarVis/text.js
@@ -20,7 +20,7 @@ var drawText = function(x, index, colInfo) {
 	});
 
     // define interactivity for the circles
-    d3.selectAll(".mvar_text")
+    d3.selectAll(".mvar_text > text")
 	.on("mouseover", function(d) {
 	    d3.select(this)
 		.transition()
@@ -29,7 +29,7 @@ var drawText = function(x, index, colInfo) {
 		       opacity: 1});
 	    hoverTable(d, d3.select(this).attr("index"));
 	});
-    d3.selectAll(".mvar_text")
+    d3.selectAll(".mvar_text > text")
 	.on("mouseout", function(d) {
 	    d3.select(this)
 		.transition()
@@ -44,6 +44,6 @@ var updateText = function(x, index) {
     var group = d3.selectAll("div")
 	.filter(function(d) { return d == index; })
     var colInfo = getColorInfo(x, index);
-    group.selectAll(".mvar_text")
+    group.selectAll(".mvar_text > text")
 	.attr("fill", function(d) { return colInfo.colorScale(d[colInfo.curCol]); })
 }
