6b7a26b58c5167dda3a10eef07b78ccccf2963f5
Kris Sankaran <sankaran.kris@gmail.com>
2015-12-14 20:05:34 -0800

    htmlwidgets js processes text options

--- inst/htmlwidgets/plot_mvar_d3.js
+++ inst/htmlwidgets/plot_mvar_d3.js
@@ -8,12 +8,14 @@ HTMLWidgets.widget({
     },
 
     renderValue: function(el, x, instance) {
+	console.log(x);
 	setupElems(el, x.length, instance.width);
 	for(i in d3.range(x.length)) {
-	    cur_x = HTMLWidgets.dataframeToD3(x[i]);
+	    cur_x = HTMLWidgets.dataframeToD3(x[i].data);
 	    createInput(el, cur_x, i);
 	    setupSVG(el, instance.width, instance.height, i, x.length);
-	    drawScatter(cur_x, i);
+	    drawScatter(cur_x, i, x[i].type);
+	    console.log(x[i].type);
 	    makeTable(el, cur_x, i);
 	}
     }
