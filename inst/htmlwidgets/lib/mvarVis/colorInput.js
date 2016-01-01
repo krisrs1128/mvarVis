e75d00d0b8ed94bc89d5e92bc710235bc793bac5
Kris Sankaran <sankaran.kris@gmail.com>
2016-01-01 15:31:05 -0800

    Add function for quantitative-only input

--- inst/htmlwidgets/lib/mvarVis/colorInput.js
+++ inst/htmlwidgets/lib/mvarVis/colorInput.js
@@ -1,6 +1,40 @@
+// Extract quantitative variables from an array
+// Args:
+//   x0: An object with some quantitative keys, potentially.
+// Returns:
+//   An array whose elements are the keys of quantiative variables in x.
+var getQuantiVars = function(x0) {
+    var quantiVars = [];
+    for(curKey in x0) {
+	var z = x0[curKey];
+	if(isNumeric(z)) {
+	    quantiVars = quantiVars.concat(curKey);
+	}
+    }
+    return quantiVars;
+}
+
+// Create an input selection for only quantitative variables
+// Args:
+//   el: The element onto which to add the input.
+//   x: An array containing the data.
+//   index: The panel within el onto which to draw the current input.
+// Returns:
+//   null, but as a side effect adds a input to el.
+var createQuantiInput = function(el, x, index) {
+    // create a dropdown selection for quantitative features
+    var quantiVars = getQuantiVars(x[0]);
+    var quantiX = d3.range(x.length).map(function(z) { return {} });
+    for(var j in quantiVars) {
+	for(var k in d3.range(x.length)) {
+	    quantiX[k][quantiVars[j]] = x[k][quantiVars[j]];
+	}
+    }
+    createInput(el, quantiX, index);
+}
 
 var createInput = function(el, x, index) {
-    // create a button for coloring the points
+    // create a dropdown selection
     var select = d3.select(el)
 	.selectAll("div")
 	.filter(function(d) { return d == index; } )
