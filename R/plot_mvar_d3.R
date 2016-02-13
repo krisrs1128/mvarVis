4aaa5bfefaadd4ba1c2ec4b18102ff597b9f96da
Kris Sankaran <sankaran.kris@gmail.com>
2016-02-12 19:50:01 -0800

    merge defaults in a separate function

--- R/plot_mvar_d3.R
+++ R/plot_mvar_d3.R
@@ -1,32 +1,57 @@
-#' Interactive multivariate analysis plots
+
+#' @title Merge default opts for mvar htmlwidgets plots
+#' @param opts A potentially partially specified list, to fill in with defaults.
+#' Currently supported options are, \cr
+#'   $height How tall should each individual table plot be? \cr
+#'   $width How wide should each individual table plot be? \cr
+#'   $asp If one of height or width is ommitted, what is the aspect ratio to
+#'   use to fill that in? \cr
+#'   $types What types of figure should be shown, for each table? Defaults to
+#'   points for all tables.
+#' @return A version of opts with defaults filled in.
+#' @export
+merge_mvar_d3_defaults <- function(opts = list(), mvar_object) {
+  default_opts <- list()
+
+  # aspect ratio (height / width) defaults to ratio of top two eigenvalues
+  if(is.null(opts$asp) & length(mvar_object@eig) >= 2) {
+    default_opts$asp <- mvar_object@eig[2] / mvar_object@eig[1]
+  } else {
+    default_opts$asp <- 1
+  }
+
+  ref_asp <- ifelse(is.null(opts$asp), default_opts$asp, opts$asp)
+  default_opts$width <- ifelse(is.null(opts$width), 500, opts$width)
+  default_opts$height <- ifelse(is.null(opts$height),
+                                ref_asp * default_opts$width,
+                                opts$height)
+
+  # plot points for all the tables
+  default_opts$types <- rep("point", length(mvar_object@table))
+  opts <- opts[!sapply(opts, is.null)]
+  modifyList(default_opts, opts)
+}
+
+#' @title Interactive multivariate analysis plots
 #' @examples
 #' library("FactoMineR")
 #' data(wine)
 #' wine_mfa <- ordi(wine[, -c(1:2)], "MFA", wine[, 1:2], group = c(5,3,10,9,2),
 #'                  type = rep("s",5), graph = F)
 #' plot_mvar_d3(wine_mfa)
-#' plot_mvar_d3(wine_mfa, c("text", "arrow"), height = 500)
+#' plot_mvar_d3(wine_mfa, c("text", "arrow"), width = 500)
 #' data(hobbies)
 #' hobbies_mca <- ordi(hobbies[, 1:18], method = "MCA",
 #'                     rows_annot = hobbies[, 19:23], graph = F)
-#' plot_mvar_d3(hobbies_mca, height = 500)
+#' plot_mvar_d3(hobbies_mca, width = 500, asp = 1)
 #' @importFrom htmlwidgets createWidget
+#' @importFrom magrittr %>%
 #' @export
-plot_mvar_d3 <- function(mvar_object, types = NULL, width = NULL, asp = NULL,
-                         height = 200) {
-  if(is.null(asp)) {
-    asp <- ifelse(length(mvar_object@eig) > 2,
-                  mvar_object@eig[1] / mvar_object@eig[2], 1)
-  }
-  if(is.null(width)) {
-    width <- asp * height * length(mvar_object@table)
-  }
-  if(is.null(types)) {
-    types <- rep("point", length(mvar_object@table))
-    if(length(types) >= 2) {
-      types[2] <- "text"
-    }
-  }
+plot_mvar_d3 <- function(mvar_object, types = NULL, height = NULL, asp = NULL,
+                         width = NULL) {
+  opts <- list(types = types, width = width, asp = asp, height = height) %>%
+    merge_mvar_d3_defaults(mvar_object)
+  print(opts)
 
   x <- list()
   for(table_ix in seq_along(mvar_object@table)) {
@@ -35,11 +60,11 @@ plot_mvar_d3 <- function(mvar_object, types = NULL, width = NULL, asp = NULL,
     cur_coord <- cbind(cur_coord, running_cosines(cur_coord))
     cur_ann <- mvar_object@table[[table_ix]]@annotation
     x[[table_ix]] <- list(data = data.frame(cur_coord, cur_ann),
-                          type = types[table_ix])
+                          type = opts$types[table_ix])
   }
 
-  createWidget(name = "plot_mvar_d3", x = x, width = width, height = height,
-               package = "mvarVis")
+  createWidget(name = "plot_mvar_d3", x = x, width = opts$width,
+               height = opts$height, package = "mvarVis")
 
 }
 
