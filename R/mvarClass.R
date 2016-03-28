36a9b14a2defc60a76c433e0c33bbd13ba065c9a
Kris Sankaran <sankaran.kris@gmail.com>
2016-03-27 18:07:05 -0700

    fix miscellaneous broken imports and references in bootstrapping code

--- R/mvarClass.R
+++ R/mvarClass.R
@@ -57,7 +57,7 @@ check_table <- function(object) {
   # Check that table is a list of mvarLayer objects
   class_is_mvar_layer <- sapply(object, function(x) class(x)=="mvarLayer")
   if(!all(class_is_mvar_layer)) {
-    errors <- paste0("Elements ", which(!class_is_mvar_layer), 
+    errors <- paste0("Elements ", which(!class_is_mvar_layer),
                      " are not valid mvarLayer objects.")
   }
   return(TRUE)
@@ -65,31 +65,31 @@ check_table <- function(object) {
 ################################################################################
 ################################################################################
 #' @title Convert mvarBootTable to mvarTable
-#' 
+#'
 #' @description Converts mvarBootTable object into 2 mvarTable objects
 #' one for center and one for bootstrap samples.
 mvar_boot2Table <- function(mvarBootTab) {
-  if (class(object) != "mvarBootTable") 
+  if (class(mvarBootTab) != "mvarBootTable")
     stop("Input object must of of class mvarBootTable")
   nBoots <- length(mvarBootTab@boot)
   bootEigs <- lapply(1:nBoots, function(i) mvarBootTab@boot[[i]]@eig)
   min_n_eig <- min(sapply(bootEigs, function(x) length(x)))
   bootEigs <- lapply(bootEigs, function(x) x[1:min_n_eig])
   meanBootEigs <- colMeans(do.call(rbind, bootEigs))
-  return(new("mvarTable", 
-        table = merge_tables(mvarBootTab@boot), 
+  return(new("mvarTable",
+        table = merge_tables(mvarBootTab@boot),
         eig = meanBootEigs))
 }
 ################################################################################
 ################################################################################
-#' @title Merge tables 
-#' 
+#' @title Merge tables
+#'
 #' @description Merge tables from the list of mvarTable objects
 merge_tables <- function(mvarTableList){
   firstTab <- mvarTableList[[1]]@table
   for (tabIDX in 1:length(firstTab)) {
     curAnnot <- firstTab[[tabIDX]]@annotation
-    firstTab[[tabIDX]]@annotation <- cbind(curAnnot, 
+    firstTab[[tabIDX]]@annotation <- cbind(curAnnot,
                                            bootIDX =rep(1, nrow(curAnnot)))
   }
   superTable <- firstTab
@@ -99,9 +99,9 @@ merge_tables <- function(mvarTableList){
     for (tabIDX in 1:length(newTable)) {
       curAnnot <- newTable[[tabIDX]]@annotation
       newAnnot <- cbind(curAnnot, bootIDX = rep(i, nrow(curAnnot)))
-      superTable[[tabIDX]]@coord <- rbind(superTable[[tabIDX]]@coord, 
+      superTable[[tabIDX]]@coord <- rbind(superTable[[tabIDX]]@coord,
                                           newTable[[tabIDX]]@coord)
-      superTable[[tabIDX]]@annotation <- rbind(superTable[[tabIDX]]@annotation, 
+      superTable[[tabIDX]]@annotation <- rbind(superTable[[tabIDX]]@annotation,
                                                newAnnot)
     }
   }
--- R/plot_mvar_from_opts.R
+++ R/plot_mvar_from_opts.R
@@ -9,6 +9,7 @@
 #'    i^th table in the \code{mvar_object}. See \code{plot_table()} for a
 #'    description of what these need to contain.
 #' @return p A ggplot object mapping the layers specified in the arguments.
+#' @importFrom ggplot2 scale_shape_manual guides guide_legend
 #' @export
 plot_mvar_from_opts <- function(mvar_object, opts = NULL, opts_center = NULL) {
   if(is.null(opts)) {
@@ -22,18 +23,18 @@ plot_mvar_from_opts <- function(mvar_object, opts = NULL, opts_center = NULL) {
     if(!is.na(mvar_object@eig[1])) {
       p <- add_eigenvalue_info(mvar_object@eig, p, opts)
     }
-  } else if (class(mvar_object) == "mvarBootTable") { 
+  } else if (class(mvar_object) == "mvarBootTable") {
     mvar_center <- mvar_object@center
     mvar_boot <- mvar_boot2Table(mvar_object)
-    center_opts <- opts$center 
-    boot_opts <- opts$boot 
-    
+    center_opts <- opts$center
+    boot_opts <- opts$boot
+
     for(cur_table in 1:length(mvar_center@table)) {
       p <- plot_table(mvar_boot@table[[cur_table]], boot_opts[[cur_table]], p, cur_table)
       if (!("shape" %in% names(center_opts[[cur_table]]$aes_list)))
         center_opts[[cur_table]]$non_aes_list$shape <- 21
       p <- plot_table(mvar_center@table[[cur_table]], center_opts[[cur_table]], p, cur_table) +
-        scale_shape_manual(values = c(21, 22, 23, 24, 25)) + 
+        scale_shape_manual(values = c(21, 22, 23, 24, 25)) +
         guides(fill = guide_legend(override.aes = list(shape = 21)))
     }
     if(!is.na(mvar_boot@eig[1])) {
